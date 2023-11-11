541f13ee89006c5e4d2dbc661a86f73c12c4f689
nemu-haibane <61283676+nemu-haibane@users.noreply.github.com>
2023-11-11 18:05:42 +0000

    Add files via upload
    changed to my own parser

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-import re
-from datetime import datetime
 from csv import DictWriter
-
 from bs4 import BeautifulSoup
 from requests import get
 
 
-new = f'/feed/?q=FastAPI'
+new = 'catalog/?q=precure&how=r'
 with open('base.csv', mode='w', encoding='utf8') as f:
-    tt = DictWriter(f, fieldnames=['date', 'title', 'link', 'text'], delimiter=';')
+    tt = DictWriter(f, fieldnames=['title', 'link'], delimiter=';')
     tt.writeheader()
-    while new:
-        row = {}
-        res = get(f'https://pythondigest.ru{new}')
-        soup = BeautifulSoup(res.text, 'html.parser')
-        for tag in soup.find_all('div', class_='item-container'):
-            title = tag.find(rel=['nofollow'])
-            row['title'] = title.get_text()
-            row['link'] = title.get('href')
-            dat = tag.find('small')
-            d1 = re.search(r'\d{2}\.\d{2}\.\d{4}', dat.get_text())[0]
-            d2 = datetime.strptime(d1, '%d.%m.%Y').date()
-            row['date'] = d2
-            row['text'] = ''.join([x.get_text() for x in tag.find_all('p')])
-            tt.writerow(row)
-        ss = soup.find('ul', class_='pagination pagination-sm')
-        p = ss.find_all('li')[-1]
-        new = p.a.get('href')
+    row = {}
+    res = get('https://diskomir.ru/' + new)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    for tag in soup.find_all('li'):
+        tag = str(tag).split('><')[0]
+        for i, t in enumerate(tag.split('<a')):
+            t = t[t.find('href') + 5: t.find('a>') - 16]
+            if i > 2:
+                row['link'] = t.split('">')[0][1:]
+                row['title'] = t.split('">')[1][36:-18]
+                tt.writerow(row)
